40390c75a28e55289345af7665aac8338129c390
Minyoung <sinkuu@naver.com>
2023-05-09 19:43:27 +0200

    Fini l'étape 04, à modifier

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,13 @@ const slides = [
 	}
 ]
 
+//le bullet point
 const banner = document.getElementById("banner");
+const bannerImg = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
 const arrowRight = document.querySelector(".arrow_right");
 const arrowLeft = document.querySelector(".arrow_left");
 const dot = document.querySelectorAll(".dot");
-
-
 
 // le bullet point actif au suivant
 let clicks = 0;
@@ -31,17 +32,38 @@ function rightControler() {
 	if(clicks < dot.length -1) {
 		dot[clicks].classList.remove("dot_selected");
 		clicks += 1;
+		//changer l'images par l'actif au suivant
+		const chosenImg = slides[clicks].image;
+		bannerImg.src = `./assets/images/slideshow/${chosenImg}`;
+		//changer le tagLine par l'actif au suivant
+		const chosenTxt = slides[clicks].tagLine;
+		tagLine.innerHTML = chosenTxt;
 		dot[clicks].classList.add("dot_selected");
 	} else {
-		dot[3].classList.remove("dot_selected");
+		clicks = dot.length - 1;
+		dot[clicks].classList.remove("dot_selected");
 		clicks = 0;
+		const chosenImg = slides[clicks].image;
+		bannerImg.src = `./assets/images/slideshow/${chosenImg}`;
+		const chosenTxt = slides[clicks].tagLine;
+		tagLine.innerHTML = chosenTxt;
 		dot[clicks].classList.add("dot_selected");}
 }
 
-function leftControler(event) {
-	console.log(event);
+// le bullet point actif au précédent
+function leftControler() {
+	dot[clicks].classList.remove("dot_selected");
+	dot[clicks-1].classList.add("dot_selected");
+	clicks = clicks - 1;
+	const chosenImg = slides[clicks].image;
+	bannerImg.src = `./assets/images/slideshow/${chosenImg}`;
+	const chosenTxt = slides[clicks].tagLine;
+	tagLine.innerHTML = chosenTxt;
 }
 
 
 arrowRight.addEventListener("click", rightControler);
 arrowLeft.addEventListener("click", leftControler);
+
+
+
